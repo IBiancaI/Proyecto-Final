fed0ee95a1bf1b80fecccafc9991cfaa1a530bac
Santiago Tolentino <santinicolino23@gmail.com>
2025-11-24 11:42:30 -0600

    PEDIR-ENTERO

--- src/proyecto.py
+++ src/proyecto.py
@@ -1,8 +1,19 @@
     # ---PROYECTO FINAL---
 
 
-def pedir_entero ():
-
+def pedir_entero (prompt,minimo=None, Maximo=None):
+    while True:
+        try:
+            v= int(input(prompt))
+            if minimo is not None and v < minimo:
+                print(f"(ERROR) Debe ser >= {minimo}.")
+                continue
+            if Maximo is not None and v > Maximo:
+                print(f"(ERROR) Debe ser <= {Maximo}.")
+                continue
+            return v
+        except ValueError:
+            print("Entrafa invalida. Ingresa un numero entero.")
 
 def pedir_float ():
 
