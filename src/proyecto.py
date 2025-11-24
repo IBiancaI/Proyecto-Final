7658ea025fac96d32c27caab076faf7d556e2605
Bianca <al07179721@tecmilenio.mx>
2025-11-24 12:30:09 -0600

    registro de ventas

--- src/proyecto.py
+++ src/proyecto.py
@@ -11,6 +11,12 @@ def mostrar_menu_principal():
 
 
 def resistrar_ventas ():
+    global total_general
+    num_clientes = pedir_entero ("¿Cuántos clientes se registrarán hoy? (0 para volver): ", minimo=0)
+    if num_clientes == 0:
+        return
+    
+    for i
 
 
 def determinar_mas_vendido ():
