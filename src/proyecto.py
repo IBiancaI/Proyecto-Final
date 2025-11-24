03a858ae4dab56690b41e622d8f4827c13875ab3
Bianca <al07179721@tecmilenio.mx>
2025-11-24 11:35:05 -0600

    defs

--- src/proyecto.py
+++ src/proyecto.py
@@ -1,1 +1,25 @@
     # ---PROYECTO FINAL---
+
+
+def pedir_entero ():
+
+
+def pedir_float ():
+
+
+def mostrar_menu_principal():
+
+
+def resistrar_ventas ():
+
+
+def determinar_mas_vendido ():
+
+
+def mostrar_resumen ():
+
+
+def corte_de_caja ():
+
+
+def main ():
